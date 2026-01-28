2189de17819b80f99f890e609936884f665ce03b
RanulJaya <ranul.jaya@gmail.com>
2026-01-29 02:12:36 +1300

    changed to port 443

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // import './src/index.js'
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 443
 const path = require('path')
 // const fileName = path.join(__dirname)
 const bodyparser = require('body-parser')
