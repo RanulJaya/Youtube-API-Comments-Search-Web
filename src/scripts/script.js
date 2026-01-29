7e3a1975f316d67260f24001e50ecd31aaffa9b5
RanulJaya <ranul.jaya@gmail.com>
2026-01-29 19:20:41 +1300

    testing the server on aws

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,24 +1,24 @@
 import '../css/styles.css'
 
 
-async function testing() {
-    const url = 'http://13.236.90.86:443/getURL'
+// async function testing() {
+//     const url = 'http://13.236.90.86:443/getURL'
 
-    try {
-        const response = await fetch(url)
-        if(!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
-        }
+//     try {
+//         const response = await fetch(url)
+//         if(!response.ok) {
+//             throw new Error(`Response status: ${response.status}`);
+//         }
 
-        const result = await response.json()
+//         const result = await response.json()
 
-        console.log(JSON.stringify(result))
-    } catch (error) {
-        console.log(error.message);
-    }
-}   
+//         console.log(JSON.stringify(result))
+//     } catch (error) {
+//         console.log(error.message);
+//     }
+// }   
 
-testing()
+// testing()
 
 function test() {
     console.log("work")
